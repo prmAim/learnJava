5952efa47162e8e4ba4084fe9e3621426dcee649
iankudinov <iankudinov@diasoft.ru>
2021-07-27 20:23:36 +0500

    Домашнее задание Java 2 к уроку 3 от 23.07.2021

---
+++ src/level2/lesson3/Main.java
@@ -0,0 +1,47 @@
+package level2.lesson3;
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    //Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и вывести список уникальных слов,
+    //из которых состоит массив (дубликаты не считаем). Посчитать сколько раз встречается каждое слово.
+    List<String> list = new ArrayList<>(Arrays.asList(
+            "Привет",
+            "мир",
+            "Тревога",
+            "Предупреждение",
+            "Внимание",
+            "Опасность",
+            "Авария",
+            "Опасность",
+            "Предупреждение",
+            "Авария",
+            "мир",
+            "Авария",
+            "мир",
+            "Опасность",
+            "Авария",
+            "Опасность"
+    ));
+
+    Map<String, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < list.size(); i++) {
+      map.put(list.get(i), map.getOrDefault(list.get(i), 0) + 1);
+    }
+
+    map.forEach((key, value) -> System.out.println("Слово <" + key + "> встречание " + value + " раз."));
+
+    // Проверка класса Phonebook
+    Phonebook ph = new Phonebook();
+    ph.add("Добрыня", 2267225);
+    ph.add("Добрыня", 2550577);
+    ph.add("Добрыня", 2267225);
+    ph.add("Илья", 2460600);
+
+    System.out.println("Проверка справочника <Добрыня> = " + ph.get("Добрыня"));
+    System.out.println("Проверка справочника <Илья> = " + ph.get("Илья"));
+    System.out.println("Проверка справочника <Соловей> = " + ph.get("Соловей"));
+  }
+}
---
+++ src/level2/lesson3/Phonebook.java
@@ -0,0 +1,31 @@
+package level2.lesson3;
+
+import java.util.*;
+
+/**
+ * Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных номеров.
+ */
+class Phonebook {
+  private Map<String, Set<Integer>> phBook = new HashMap<>();
+
+  /**
+   * Добавление в телефонный справочник Имя и его номер телефона
+   */
+  void add(String name, int phoneNumber) {
+    if (phBook.get(name) == null) {
+      phBook.put(name, new HashSet<>(Arrays.asList(phoneNumber)));
+    } else {
+      phBook.get(name).add(phoneNumber);
+    }
+  }
+
+  /**
+   * С помощью метода get() искать номер телефона по фамилии.
+   * * Следует учесть, что под одной фамилией может быть несколько телефонов, тогда при запросе такой фамилии должны
+   * * выводиться все телефоны.
+   */
+  Set<Integer> get(String name) {
+    return phBook.getOrDefault(name, new HashSet<>(Arrays.asList(0)));
+  }
+
+}
